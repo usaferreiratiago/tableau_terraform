d10c6193cdb2f0f91748b0915812870772a6d26b
Tiago Ferreira <usaferreiratiago@gmail.com>
2026-07-19 10:21:33 -0300

    tableau_terraform

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -1,5 +1,6 @@
 output "instance_profile_name" {
-  value = aws_iam_instance_profile.tableau.name
+  description = "The IAM instance profile name for the Tableau EC2"
+  value       = aws_iam_instance_profile.tableau_server.name
 }
 
 output "role_name" {
