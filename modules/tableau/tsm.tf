e12e4b157490d6d5218d88b7781904b1dd0d784b
Tiago Ferreira <usaferreiratiago@gmail.com>
2026-07-19 11:00:11 -0300

    tableau_terraform

--- modules/tableau/tsm.tf
+++ modules/tableau/tsm.tf
@@ -1,11 +1,24 @@
+locals {
+  # Connection details for the remote-exec provisioner
+  connection = {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file(var.ssh_private_key_path) # Ensure this is passed as a variable
+    host        = var.instance_private_ip
+  }
+  
+  script_path = "/home/ec2-user/scripts"
+}
+
 resource "terraform_data" "tsm_config" {
   depends_on = [terraform_data.install]
+  
   connection = local.connection
 
   provisioner "remote-exec" {
     inline = [
       "chmod +x ${local.script_path}/configure-tsm.sh",
-      "${local.script_path}/configure-tsm.sh ${var.tableau_admin_user} ${var.tableau_admin_pass}"
+      "${local.script_path}/configure-tsm.sh ${var.tableau_admin_username} ${var.tableau_admin_password}"
     ]
   }
 }
