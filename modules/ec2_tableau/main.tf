95132f282fe01dd8351bd1a509bc8c4384994258
Tiago Ferreira <usaferreiratiago@gmail.com>
2026-07-20 08:34:30 -0300

    tableau_terraform

--- modules/ec2_tableau/main.tf
+++ modules/ec2_tableau/main.tf
@@ -32,27 +32,3 @@ resource "aws_instance" "this" {
     ignore_changes = [ami]
   }
 }
-
-resource "aws_instance" "this" {
-  ami                    = var.ami_id
-  instance_type          = var.instance_type
-  subnet_id              = var.subnet_id
-  private_ip             = var.instance_ip
-  vpc_security_group_ids = var.security_group_ids
-  key_name               = var.key_pair_name
-
-  metadata_options {
-    http_endpoint = "enabled"
-    http_tokens   = "required"
-  }
-
-  root_block_device {
-    encrypted             = true
-    volume_type           = "gp3"
-    delete_on_termination = true
-  }
-
-  tags = {
-    Name = var.instance_name
-  }
-}
